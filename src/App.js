b5af7039e2318b0ecaef08e87f7fce5d4112c70f
christian <christiandavidphoto@gmail.com>
2018-02-15 16:29:51 -0500

    refactor/href-added
    missing '.href'

--- src/App.js
+++ src/App.js
@@ -147,7 +147,7 @@ class App extends Component {
         ) : (
           <button
             onClick={() => {
-              window.location = window.location.includes('localhost')
+              window.location = window.location.href.includes('localhost')
                 ? 'http://localhost:8888/login'
                 : 'https://better-playlists-cdt.herokuapp.com/login';
             }}
